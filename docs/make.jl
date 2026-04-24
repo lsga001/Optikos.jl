907ef84d891dc1fb728359be5016b34703402974
lsga001 <leo.ga@live.com>
2026-04-23 19:43:48 -0600

    Changed some documentation code

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,6 @@
 using Documenter, Optikos
 
-#makedocs(sitename="My Documentation", remotes = nothing)
-
 makedocs(sitename="Optikos.jl")
 deploydocs(
     repo = "github.com/lsga001/Optikos.jl.git",
 )
-
-pages = [
-  "Home" => "index.md",
-  "Getting started" => "example.md",
-]
