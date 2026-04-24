10ccbb8cea722778248141ae0abb3138d38999e5
lsga001 <leo.ga@live.com>
2026-04-23 18:52:03 -0600

    Added documenter github actions support

--- docs/build/search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"category":"section","location":"#Optikos.jl-documentation","page":"Optikos.jl documentation","text":"","title":"Optikos.jl documentation"},{"category":"section","location":"#Functions","page":"Optikos.jl documentation","text":"","title":"Functions"},{"category":"section","location":"#Index","page":"Optikos.jl documentation","text":"","title":"Index"},{"category":"method","location":"#Optikos.succ-Tuple{Any}","page":"Optikos.jl documentation","text":"succ(x)\n\nReturn the number x plus 1.\n\n\n\n\n\n","title":"Optikos.succ"}]
-}
--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,13 @@
 using Documenter, Optikos
 
-makedocs(sitename="My Documentation", remotes = nothing)
+#makedocs(sitename="My Documentation", remotes = nothing)
+
+makedocs(sitename="Optikos.jl")
+deploydocs(
+    repo = "github.com/lsga001/Optikos.jl.git",
+)
+
+pages = [
+  "Home" => "index.md",
+  "Getting started" => "example.md",
+]
